e484424e7869ee1cd6737205d4095a56af26f817
Robin Andeer <robin.andeer@gmail.com>
2015-01-16 08:44:23 +0100

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,20 +6,6 @@ from taboo._compat import split
 from taboo.utils import namebase, track_rows, startswith
 
 
-def test_namebase():
-  # test absolute path
-  assert namebase('/var/pass.txt') == 'pass'
-
-  # test filename with extension
-  assert namebase('config.ini') == 'config'
-
-  # test relative path
-  assert namebase('folder/some_file.py') == 'some_file'
-
-  # test "multiple" extensions
-  assert namebase('alignment.bam.bai') == 'alignment.bam'
-
-
 def test_track_rows():
   lines = ['#header\trow\there', '#another\theader', 'lost\tdata', 'data!']
   rows = map(split(sep='\t'), lines)
@@ -27,8 +13,3 @@ def test_track_rows():
 
   assert headers == lines[:2]
   assert next(rows) == [lines[3]]
-
-
-def test_startswith():
-  assert startswith('#CHROM', '#CHROM\tRSNUMBER\tPOS') is True
-  assert startswith('i', 'ePod') is False
