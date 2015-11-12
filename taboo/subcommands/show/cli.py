9ee5929730dd07c2033411f4bd93cf80e976a609
Robin Andeer <robin.andeer@gmail.com>
2015-11-12 11:00:54 +0100

    allow listing all samples in show command include "experiment" in show output, fix #14

--- taboo/subcommands/show/cli.py
+++ taboo/subcommands/show/cli.py
@@ -14,23 +14,27 @@ def stringify_genotypes(genotypes):
 
 @click.command()
 @click.argument('samples', nargs=-1, type=str)
+@click.option('-s', '--skip', default=0)
 @click.option('-r', '--references', type=click.File('r'), required=True)
 @click.pass_context
-def show(context, samples, references):
+def show(context, samples, skip, references):
     """Show genotype strings for multiple samples."""
     query = context.parent.store.session.query
-    db_samples = query(Sample).filter(Sample.sample_id.in_(samples))
+    db_samples = query(Sample)
+    if samples:
+        db_samples = db_samples.filter(Sample.sample_id.in_(samples))
 
     rsnumber_references = taboo.rsnumbers.read(references)
     reference_dict = taboo.rsnumbers.dictify(rsnumber_references)
 
     all_rsnumbers = taboo.store.unique_rsnumbers(query)
     rsnumber_columns = '\t'.join(all_rsnumbers)
-    click.echo("#id\tsample_id\t{}".format(rsnumber_columns))
+    click.echo("#id\tsample_id\texperiment\t{}".format(rsnumber_columns))
 
+    template = ("{sample.id}\t{sample.sample_id}\t{sample.experiment}"
+                "\t{genotype_str}")
     for sample in db_samples:
         genotypes = taboo.match.fill_forward(all_rsnumbers, reference_dict,
                                              sample.genotypes)
         genotype_str = '\t'.join(str(genotype) for genotype in genotypes)
-        click.echo("{sample.id}\t{sample.sample_id}\t{genotype_str}"
-                   .format(sample=sample, genotype_str=genotype_str))
+        click.echo(template.format(sample=sample, genotype_str=genotype_str))
