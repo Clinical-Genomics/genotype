2fc1ca03409344b9347f58b9bc991291ebd5f66a
Robin Andeer <robin.andeer@gmail.com>
2015-11-13 10:03:34 +0100

    update tasks and change log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,34 @@
 # Change Log
 All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
 
-## 0.2.0 - 2015-06-26
+## [0.4.0] - 2015-11-13
+### Added
+- use logging module in CLI
+- store sex prediction from excel report in database
+- force option for re-uploading existing samples
+
+### Changed
+- Failed uploads are only skipped (upload)
+- List samples from database without ids
+- "experiment" now included in "show" output
+
+## [0.3.1] - 2015-09-25
+### Fixed
+- Support latest `vcf_parser`
+
+## [0.3.0] - 2015-09-08
+### Changed
+- Require a RS reference for matching
+- Remove dependencies
+
+## [0.2.0] - 2015-06-26
+### Changed
 - Change ``Sample.origin`` to ``Sample.experiment``
+
+### Added
 - Add ``Sample.source``
 - Update interface accordingly
 
-## 0.0.1 - 2014-12-09
+## [0.0.1] - 2014-12-09
 First draft release. Everything is new.
--- tasks.py
+++ tasks.py
@@ -8,59 +8,50 @@ from invoke.util import log
 
 @task
 def test():
-  """test - run the test runner."""
-  run('python setup.py test', pty=True)
-
-
-@task(name='test-all')
-def testall():
-  """test-all - run tests on every Python version with tox."""
-  run('tox')
+    """test - run the test runner."""
+    run('python setup.py test', pty=True)
 
 
 @task
 def clean():
-  """clean - remove build artifacts."""
-  run('rm -rf build/')
-  run('rm -rf dist/')
-  run('rm -rf taboo.egg-info')
-  run('find . -name __pycache__ -delete')
-  run('find . -name *.pyc -delete')
-  run('find . -name *.pyo -delete')
-  run('find . -name *~ -delete')
-
-  log.info('cleaned up')
+    """clean - remove build artifacts."""
+    run('rm -rf build/')
+    run('rm -rf dist/')
+    run('rm -rf taboo.egg-info')
+    run('find . -name __pycache__ -delete')
+    run('find . -name *.pyc -delete')
+    run('find . -name *.pyo -delete')
+    run('find . -name *~ -delete')
+    log.info('cleaned up')
 
 
 @task
 def lint():
-  """lint - check style with flake8."""
-  run('flake8 taboo tests')
+    """lint - check style with flake8."""
+    run('flake8 taboo tests')
 
 
 @task
 def coverage():
-  """coverage - check code coverage quickly with the default Python."""
-  run('coverage run --source taboo setup.py test')
-  run('coverage report -m')
-  run('coverage html')
-  run('open htmlcov/index.html')
-
-  log.info('collected test coverage stats')
+    """coverage - check code coverage quickly with the default Python."""
+    run('coverage run --source taboo setup.py test')
+    run('coverage report -m')
+    run('coverage html')
+    run('open htmlcov/index.html')
+    log.info('collected test coverage stats')
 
 
 @task(clean)
 def publish(test=False):
-  """publish - package and upload a release to the cheeseshop."""
-  run('python setup.py register bdist_wheel upload')
-  run('python setup.py register sdist upload')
-
-  log.info('published new release')
+    """publish - package and upload a release to the cheeseshop."""
+    run('python setup.py register bdist_wheel upload')
+    run('python setup.py register sdist upload')
+    log.info('published new release')
 
 
 @task
 def docs():
-  """docs - build Sphinx documentation and display in browser."""
-  run('make -C docs html')
-  run('open docs/_build/html/index.html')
-  log.info('built and displayed documentation')
+    """docs - build Sphinx documentation and display in browser."""
+    run('make -C docs html')
+    run('open docs/build/html/index.html')
+    log.info('built and displayed documentation')
