7599ee70f5e79d3bb4032911d951ccbc9c80c312
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-09-11 11:44:37 +0200

    Bump version: 2.2.4 → 2.2.5

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.2.4',
+    version='2.2.5',
 
     description=("Taboo provides an automated pipipeline for comparing"
                  "genotypes from different assays."),
