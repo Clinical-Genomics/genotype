e2246c3151a7cbb2563360af347f59905b851a2c
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-09-17 13:58:49 +0200

    Updates load tests

--- tests/load/test_load_mixins.py
+++ tests/load/test_load_mixins.py
@@ -1,16 +1,23 @@
-# -*- coding: utf-8 -*-
+"""Tests for the load mixins"""
+
+from alchy import Manager
+
 from genotype.store import api
 from genotype.store.models import Analysis, Genotype, Sample
 
 
-def test_add_analysis(sample_db):
+def test_add_analysis(genotype_db: Manager):
+    """Test to add an analysis to a empty database"""
     # GIVEN a new Analysis to be added to an empty database
     assert Analysis.query.first() is None
+    assert Sample.query.first() is None
     sample_id = "sample"
     genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
     new_analysis = Analysis(type="genotype", sample_id=sample_id, genotypes=genotypes)
+
     # WHEN adding it to the database
-    api.add_analysis(sample_db, new_analysis)
+    api.add_analysis(genotype_db, new_analysis)
+
     # THEN it should work and add analysis, sample, and genotypes
     assert Sample.query.count() == 1
     assert Sample.query.first().id == sample_id
@@ -19,32 +26,71 @@ def test_add_analysis(sample_db):
     assert Genotype.query.count() == 1
     assert Genotype.query.all() == Analysis.query.first().genotypes
 
+
+def test_add_analysis_when_exist(genotype_db: Manager):
+    """Test to add an analysis when analysis already exists"""
     # GIVEN an already loaded analysis and a new conflicting one
-    # see above...
+    assert Analysis.query.first() is None
+    sample_id = "sample"
+    genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
+    new_analysis = Analysis(type="genotype", sample_id=sample_id, genotypes=genotypes)
+    api.add_analysis(genotype_db, new_analysis)
+
+    # GIVEN a new analysis with the same sample
     new_genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
     newer_analysis = Analysis(
         type="genotype", sample_id=sample_id, genotypes=new_genotypes, sex="female"
     )
+
     # WHEN trying to add it again (update)
-    is_saved = api.add_analysis(sample_db, newer_analysis)
+    is_saved = api.add_analysis(genotype_db, newer_analysis)
     # THEN it should return None
     assert is_saved is None
 
-    # GIVEN an old analysis
+
+def test_overwrite_existing_analysis(genotype_db: Manager):
+    # GIVEN a database with an analysis where sex is not specified
+    sample_id = "sample"
+    genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
+    new_analysis = Analysis(type="genotype", sample_id=sample_id, genotypes=genotypes)
+    api.add_analysis(genotype_db, new_analysis)
     assert Analysis.query.first().sex is None
+
+    # GIVEN a new analysis with the same sample where sex is specified
+    new_genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
+    newer_analysis = Analysis(
+        type="genotype", sample_id=sample_id, genotypes=new_genotypes, sex="female"
+    )
+
     # WHEN adding it with force flag
-    api.add_analysis(sample_db, newer_analysis, replace=True)
+    api.add_analysis(genotype_db, newer_analysis, replace=True)
+
     # THEN it should remove the old analysis before re-adding it
     assert Analysis.query.count() == 1
     assert Analysis.query.first().sex == "female"
 
+
+def test_new_comment_is_added_when_replacing(genotype_db: Manager):
     # GIVEN an old analysis with a comment and a new one
-    old_comment = Sample.query.first().comment
+    sample_id = "sample"
+    sample_obj = Sample(id=sample_id, comment="a comment")
+    genotype_db.add_commit(sample_obj)
+
+    genotypes = [Genotype(rsnumber="rs12", allele_1="A", allele_2="T")]
+    analysis = Analysis(type="genotype", sample_id=sample_id, genotypes=genotypes, sex="female")
+
+    api.add_analysis(genotype_db, analysis)
+    sample_obj = Sample.query.first()
+    old_comment = sample_obj.comment
     assert old_comment is not None
-    newest_analysis = Analysis(type="genotype", sample_id=sample_id)
+
+    new_analysis = Analysis(type="genotype", sample_id=sample_id)
+
     # WHEN replacing the old with the new analysis
-    api.add_analysis(sample_db, newest_analysis, replace=True)
+    api.add_analysis(genotype_db, new_analysis, replace=True)
+
     # THEN it should append a log message to the comment on the sample
-    new_comment = Sample.query.first().comment
+    sample_obj = Sample.query.first()
+    new_comment = sample_obj.comment
     assert old_comment in new_comment
     assert new_comment != old_comment
--- tests/load/test_load_vcf.py
+++ tests/load/test_load_vcf.py
@@ -1,18 +1,25 @@
-# -*- coding: utf-8 -*-
+"""Tests for load vcf functionality"""
+
+from pathlib import Path
+from typing import List
+
 from genotype.load import vcf as vcf_mod
-from genotype.store.models import Analysis
+from genotype.store.models import Analysis, SNP
 
 
-def test_load_vcf(bcf_path, snps):
+def test_load_vcf(bcf_path: Path, snps: List[SNP]):
+    """Test to load a vcf"""
     # GIVEN a BCF file with one sample and a list of SNP records
     sample_id = "000139T"
-    no_snps = len(snps)
+    nr_snps = len(snps)
+
     # WHEN building sequence analyses records
-    analyses = list(vcf_mod.load_vcf(bcf_path, snps))
+    analyses = list(vcf_mod.load_vcf(vcf_file=str(bcf_path), snps=snps))
+
     # THEN it should return a new analysis object and link to the sample
     assert len(analyses) == 1
     analysis = analyses[0]
     assert isinstance(analysis, Analysis)
     assert analysis.sample_id == sample_id
     assert analysis.sample is None
-    assert len(analysis.genotypes) == no_snps
+    assert len(analysis.genotypes) == nr_snps
