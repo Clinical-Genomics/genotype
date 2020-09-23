bdbfd23dc61c293b1a1d441f31a4e3b09765293b
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-09-23 09:54:08 +0200

    Fix store tests

--- tests/store/test_store_api.py
+++ tests/store/test_store_api.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
+"""Unit tests for store API"""
+
+from alchy import Manager
+
 from genotype.store.models import Sample
 
 
-def test_add_many(genotype_db):
+def test_add_many(snp_db: Manager):
     # GIVEN multiple new samples
     new_samples = [Sample(id="ADM12"), Sample(id="ADM13")]
     # WHEN added to the session
-    genotype_db.add_commit(*new_samples)
+    snp_db.add_commit(*new_samples)
     # THEN all samples should be added
     assert Sample.query.all() == new_samples
