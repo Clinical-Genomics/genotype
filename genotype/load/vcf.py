bb7abe62ac67c70abf99256d9f7cf2288d9d6190
Christian Oertlin <christian@n177-p153.eduroam.kth.se>
2023-01-11 11:30:31 +0100

    black lint improvements

--- genotype/load/vcf.py
+++ genotype/load/vcf.py
@@ -64,4 +64,6 @@ def fetch_snp(vcf: VCF, snp: SNP) -> Variant:
         variant = variants[0]
         return variant
     # weird SNP position lookup, not even possible; right?
-    raise ValueError(f"Multiple variants {len(variants)} found for SNP at position {pos_str}: {variants}.\n The associated BCF file can be found here: {vcf.fname}")
+    raise ValueError(
+        f"Multiple variants {len(variants)} found for SNP at position {pos_str}: {variants}.\n The associated BCF file can be found here: {vcf.fname}"
+        )
